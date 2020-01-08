069e6257954e4768584b3e517b35843443b66203
amir mor <amirmor75@gmail.com>
2020-01-08 17:58:08 +0200

    added Frame and protocol

---
+++ Boost_Echo_Client/src/StompMessagingProtocol.cpp
@@ -0,0 +1,32 @@
+//
+// Created by amir on 08/01/2020.
+//
+
+#include <Frame.h>
+#include "StompMessagingProtocol.h"
+
+string StompMessagingProtocol::process(Frame frame) {
+    string command=frame.getCommand();
+    string message_body=frame.getMessageBody();
+    std::unordered_map<string,string> header_map=frame.getHeaderMap();
+    if(command.compare("CONNECTED")==0){
+        printf("Login successful \n");
+        //login=true
+    }
+    if(command.compare("ERROR")==0 && command.compare("RECEIPT")==0){
+        printf("%s",message_body.c_str());
+    }
+    if(command.compare("MESSAGE")==0){
+        //SEND username has book name
+        //
+        //sends SEND frame with: taking {bookname} from {bookOwnerUsername}.
+        //
+    }
+
+    //process of keyboard
+    //add and return doesnt need frame
+    if(command.compare("SEND")==0){
+        //should add the book to the vector
+    }
+
+}
