473366ad489c552042e96e8bfce7a5d3f9bbe248
amirmor75 <amirmor75@gmail.com>
2020-01-10 11:16:02 +0200

    connections impl done

--- spl-net/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ spl-net/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -1,4 +1,35 @@
 package bgu.spl.net.srv;
 
-public class ConnectionsImpl {
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class ConnectionsImpl implements Connections<String> {
+    private Hashtable<Integer,ConnectionHandler> handlersMap;
+    private Hashtable<String, ConcurrentSkipListSet<Integer>> subscribersMap;// key- genrename, element- set of subs ids
+
+
+    @Override
+    public boolean send(int connectionId, String msg) {
+        handlersMap.get(connectionId).send(msg);
+        return true;// just for the hell of it
+    }
+
+    @Override
+    public void send(String channel, String msg) {
+        ConcurrentSkipListSet<Integer> idSet=subscribersMap.get(channel);
+        for(Integer id: idSet)
+            send(id,msg);
+
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+        try {
+            handlersMap.get(connectionId).close();
+        }
+        catch (IOException ignored){}
+    }
 }
